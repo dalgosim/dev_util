2b84b18a74f9fb491caead412f3a4a2f7f5e16e6
sangkwon <andy.sangkwon@gmail.com>
2020-08-17 07:24:55 +0900

    add date 기능 음수 오류 수정

--- util/timer.py
+++ util/timer.py
@@ -10,7 +10,10 @@ def get_now(time_format='%Y-%m-%d %H:%M:%S'):
     return datetime.datetime.now().strftime(time_format)
 
 def str2date(str_date, date_format='%Y-%m-%d'):
-    return datetime.datetime.strptime(str_date, date_format)
+    if isinstance(str_date, str):
+        return datetime.datetime.strptime(str_date, date_format)
+    else:
+        return str_date
 
 def get_yesterday(time_format='%Y-%m-%d %H:%M:%S'):
     yesterday = datetime.date.today() - datetime.timedelta(days=1)
@@ -21,4 +24,8 @@ def get_now_hour():
 
 def add_date(str_date, delta_day, date_format='%Y-%m-%d'):
     _date = str2date(str_date, date_format)
-    return (_date + datetime.timedelta(days=delta_day)).strftime(date_format)
+    if delta_day > 0:
+        _date = _date + datetime.timedelta(days=delta_day)
+    else:
+        _date = _date - datetime.timedelta(days=abs(delta_day))
+    return _date.strftime(date_format)
