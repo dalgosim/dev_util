7e22d0e6a7f35e81458e7edddfa656d1feb5fe77
sangkwon <andy.sangkwon@gmail.com>
2020-08-30 22:03:17 +0900

    get price list

--- util/common_sql.py
+++ util/common_sql.py
@@ -8,3 +8,15 @@ def get_company_list(limit=0):
     limit_q = f'''LIMIT {limit}''' if limit > 0 else ''
     query = f'''SELECT distinct cmp_cd FROM {table} {limit_q};'''
     return _mysql.select_dataframe(query, log='get_company_list')
+
+def get_recent_price(cmp_cd, days=121, order='DESC'):
+    _mysql = mysql_manager.MysqlController()
+    table = config.CONFIG.MYSQL_CONFIG.TABLES.PRICE_TABLE
+    query = f'''
+    SELECT distinct *
+    FROM {table}
+    WHERE LEFT(cmp_cd,6)="{cmp_cd}"
+    ORDER BY date {order}
+    LIMIT {days};
+    '''
+    return _mysql.select_dataframe(query, log='get_price_list')
