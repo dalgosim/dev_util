c654bec0847f2401857323a5e880d2db755c2567
dalgosim <dalgosim@gmail.com>
2021-01-10 14:49:16 +0900

    get_company_list_without_del function 추가

--- util/common_sql.py
+++ util/common_sql.py
@@ -9,14 +9,28 @@ def get_company_list(limit=0):
     query = f'''SELECT distinct cmp_cd FROM {table} {limit_q};'''
     return _mysql.select_dataframe(query, log='get_company_list')
 
-def get_recent_price(cmp_cd, days=121, order='DESC'):
+def get_recent_price(cmp_cd, last_date, days=121, order='DESC'):
     _mysql = mysql_manager.MysqlController()
     table = config.CONFIG.MYSQL_CONFIG.TABLES.PRICE_TABLE
     query = f'''
-    SELECT distinct *
+    SELECT *
     FROM {table}
-    WHERE LEFT(cmp_cd,6)="{cmp_cd}"
+    WHERE cmp_cd="{cmp_cd}"
+        AND date<="{last_date}"
     ORDER BY date {order}
     LIMIT {days};
     '''
     return _mysql.select_dataframe(query, log='get_price_list')
+
+def get_company_list_without_del():
+    _mysql = mysql_manager.MysqlController()
+    cmp_table = config.CONFIG.MYSQL_CONFIG.TABLES.COMPANY_LIST_TABLE
+    del_table = config.CONFIG.MYSQL_CONFIG.TABLES.COMPANY_DEL_LIST_TABLE
+    query = f'''
+    SELECT c.cmp_cd
+    FROM {cmp_table} c
+    LEFT JOIN {del_table} cd
+    ON c.cmp_cd=cd.cmp_cd
+    WHERE cd.cmp_cd IS NULL;
+    '''
+    return _mysql.select_dataframe(query, log='get_company_list_without_del')
