4f2b5f1f982cb3c453a8ccf83b436cfb6ca1f1eb
dalgosim <dalgosim@gmail.com>
2021-05-09 10:21:26 +0900

    config_name으로 DB속성 불러오기

--- util/config.py
+++ util/config.py
@@ -9,7 +9,7 @@ CONFIG = DotMap()
 BASIS_DATE = timer.get_now('%Y-%m-%d')
 TEST_MODE = True
 
-def load_config(run_type='test', path='config.json'):
+def load_config(config_name='REAL_DB', path='config.json'):
     global CONFIG, TEST_MODE, BASIS_DATE
 
     AUTH_PATH = './auth/mysql_auth.json'
@@ -30,16 +30,9 @@ def load_config(run_type='test', path='config.json'):
     try:
         with open(AUTH_PATH) as f:
             auth_data = json.load(f)
-
-            # 사용하는 설정만 남기고 지우기
-            TEST_MODE = False if run_type.lower() == 'real' else True
-            _logger.debug(f'===== {run_type.upper()} MODE =====')
-
-            if TEST_MODE:
-                auth_data['MYSQL_CONFIG'] = auth_data['MYSQL_SVR']['DEV_DB']
-            else:
-                auth_data['MYSQL_CONFIG'] = auth_data['MYSQL_SVR']['REAL_DB']
+            auth_data['MYSQL_CONFIG'] = auth_data['MYSQL_SVR'][config_name]
             auth_data.pop('MYSQL_SVR')
+            _logger.debug(f'Loaded database config')
     except:
         auth_data = {}
         _logger.debug('There is no auth config')
